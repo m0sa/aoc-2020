9486cbe8ff357f86f3da510483e76fc90a5fc1ec
Samo Prelog <samo.prelog@gmail.com>
2020-12-01 09:24:22 +0000

    day 1

--- tests.ts
+++ tests.ts
@@ -2,9 +2,41 @@ import {
     assertArrayIncludes,
     assertEquals,
   } from "https://deno.land/std@0.79.0/testing/asserts.ts";
-  
-Deno.test("hello world", () => {
-    const x = 1 + 2;
-    assertEquals(x, 3);
-    assertArrayIncludes([1, 2, 3, 4, 5, 6], [3], "Expected 3 to be in the array");
-});
+
+Deno.test("day 1", async () => {
+  const text = await Deno.readTextFile('inputs/day1.txt');
+  var numbers = text.split('\n').map(t => parseInt(t));
+
+  function day1part1(numbers: Array<number>) {
+    for (let i = 0; i < numbers.length; i++)
+    {
+      for (let j = i + 1; j < numbers.length; j++)
+      {
+        if (numbers[i] + numbers[j] == 2020)
+        {
+          return numbers[i] * numbers[j];
+        }
+      }
+    }
+    return NaN;
+  }
+  assertEquals(889779, day1part1(numbers));
+
+  function day1part2(numbers: Array<number>) {
+    for (let i = 0; i < numbers.length; i++)
+    {
+      for (let j = i + 1; j < numbers.length; j++)
+      {
+        for (let k = j + 1; k < numbers.length; k++)
+        {
+          if (numbers[i] + numbers[j] + numbers[k] == 2020)
+          {
+            return numbers[i] * numbers[j] * numbers[k];
+          }
+        }
+      }
+    }
+    return NaN;
+  }
+  assertEquals(76110336, day1part2(numbers));
+})
